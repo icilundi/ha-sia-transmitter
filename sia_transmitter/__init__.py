309668f7c2c66ee65cc97cb55a3fa6fb64bf56c6
lepetoc <lepetoctgp@outlook.fr>
2025-06-03 10:45:14 +0200

    properly sending webhook message

--- sia_transmitter/__init__.py
+++ sia_transmitter/__init__.py
@@ -52,16 +52,18 @@ async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> b
     async def send_sia_message(service_call: ServiceCall):
         current_server = hass.data[DOMAIN]["connection"]
         account = accounts[current_server]
-        await sia.send_sia(
-            account[CONF_HOST],
-            account[CONF_PORT],
-            account[CONF_ACCOUNT_ID],
-            service_call.data.get("timestamp"),
-            service_call.data.get("message", ""),
-        )
-        if CONF_WEBHOOK in config_entry.data:
-            await send_webhook(config_entry.data[CONF_WEBHOOK], "message", message, config_entry.data.get(CONF_WEBHOOK_DATA))
-
+        try:
+            await sia.send_sia(
+                account[CONF_HOST],
+                account[CONF_PORT],
+                account[CONF_ACCOUNT_ID],
+                service_call.data.get("timestamp"),
+                service_call.data.get("message", ""),
+            )
+            if CONF_WEBHOOK in config_entry.data:
+                await send_webhook(config_entry.data[CONF_WEBHOOK], "message", message, config_entry.data.get(CONF_WEBHOOK_DATA))
+        except Exception as e:
+            _LOGGER.error(f"Échec de connexion à {account[CONF_HOST]}: {e}")
     # async def send_sia_message(service_call: ServiceCall):
     #     """Send a message to the server and in a case of failure, retry by calling supervision"""
     #     message = service_call.data.get("message")
@@ -98,7 +100,7 @@ async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> b
     #         _LOGGER.error("Échec de l'envoi du message après plusieurs tentatives.")
 
     hass.services.async_register(DOMAIN, SERVICE_SEND_SIA_NAME, send_sia_message)
-    await send_webhook(config_entry.data[CONF_WEBHOOK], "message", "#23346|NCA", config_entry.data.get(CONF_WEBHOOK_DATA))
+    # await send_webhook(config_entry.data[CONF_WEBHOOK], "message", "#23346|NCA", config_entry.data.get(CONF_WEBHOOK_DATA))
 
     if config_entry.data[CONF_SUPERVISION]:
         async_track_time_interval(
